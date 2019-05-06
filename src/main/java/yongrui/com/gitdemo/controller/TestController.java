84a5226b64dedc1f1b77a1dea6e9a743a4cd3d78
RuiGer <498600516@qq.com>
2019-05-06 10:15:08 +0800

    4

--- src/main/java/yongrui/com/gitdemo/controller/TestController.java
+++ src/main/java/yongrui/com/gitdemo/controller/TestController.java
@@ -6,5 +6,6 @@ public class TestController {
         System.out.println("test 1");
         System.out.println("test 2");
         System.out.println("test 3");
+        System.out.println("test 4");
     }
 }
